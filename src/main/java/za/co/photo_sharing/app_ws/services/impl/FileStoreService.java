72fb848543c089debc3f8709b7483327ef53cc3a
Siyamcela Nxuseka <siya.nxuseka@gmail.com>
2020-09-01 23:37:47 +0200

    WEB-PLATFORM-00035: S3 List Objects

--- src/main/java/za/co/photo_sharing/app_ws/services/impl/FileStoreService.java
+++ src/main/java/za/co/photo_sharing/app_ws/services/impl/FileStoreService.java
@@ -2,8 +2,10 @@ package za.co.photo_sharing.app_ws.services.impl;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.util.IOUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,8 +16,7 @@ import za.co.photo_sharing.app_ws.exceptions.UserServiceException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.rmi.UnknownHostException;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -61,6 +62,31 @@ public class FileStoreService {
         } catch (AmazonServiceException | IOException e) {
             throw new IllegalStateException("Failed to download file to s3", e);
         }
+    }
+
+    public Set<String> fetchImages(String bucketName, String folder,String path){
+
+        ObjectListing objectListing = s3.listObjects(bucketName);
+        Set<String> images = new HashSet<>();
+        List<S3ObjectSummary> objectSummaries = objectListing.getObjectSummaries();
+        if (objectSummaries.size()>0){
+            objectSummaries.stream()
+                    .filter(s3ObjectSummary -> s3ObjectSummary.getKey().contains(folder))
+                    .forEach(s3ObjectSummary -> {
+                        String[] key = s3ObjectSummary.getKey().split("/");
+                        String imageKey = key[2];
+                        S3Object obj = s3.getObject(path, imageKey);
+                        byte[] bytes;
+                        try {
+                            bytes = IOUtils.toByteArray(obj.getObjectContent());
+                        } catch (Exception e) {
+                            throw new IllegalStateException("Failed to download file to s3", e);
+                        }
+                        String image = Base64.getEncoder().encodeToString(bytes);
+                        images.add(image);
+                    });
+        }
+        return images;
     }
 
 
